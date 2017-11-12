98300e261fae7832c10c511d0358c18747b5aca0
Mark Bell <me@markb.co.uk>
2017-11-12 11:16:44 +0000

    Add container for common functions and constants

---
+++ money.common/Globals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace money.common
+{
+    public static class Globals
+    {
+        public const string USER_SESSION_VARIABLE_NAME = "USER_ID";
+    }
+}
