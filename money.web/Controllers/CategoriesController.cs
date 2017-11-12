50386f5975e79d2fe4b23df4be753af292b7f8d2
Mark Bell <me@markb.co.uk>
2017-11-12 14:22:54 +0000

    Implement Category CRUD

---
+++ money.web/Controllers/CategoriesController.cs
@@ -0,0 +1,102 @@
+﻿using Dapper;
+using money.common;
+using money.web.Abstract;
+using money.web.Models;
+using money.web.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace money.web.Controllers
+{
+    public class CategoriesController : ControllerBase
+    {
+        public CategoriesController(IUnitOfWork unitOfWork, IQueryHelper db, IRequestContext context) : base(unitOfWork, db, context) { }
+
+        public ActionResult Index()
+        {
+            return View(new ListCategoriesViewModel {
+                Categories = _db.Query(conn => conn.Query<CategoryDTO>("SELECT * FROM Categories"))
+            });
+        }
+
+        public ActionResult Create()
+        {
+            return View(new CreateCategoryViewModel {
+                Accounts = AccountsSelectListItems()
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateCategoryViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Accounts = AccountsSelectListItems();
+                return View(model);
+            }
+
+            _db.InsertOrUpdate(new CategoryDTO {
+                AccountID = model.AccountID,
+                Name = model.Name
+            });
+
+            _unitOfWork.CommitChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public ActionResult Update(int id)
+        {
+            var dto = _db.Get<CategoryDTO>(id);
+
+            return View(new UpdateCategoryViewModel {
+                ID = dto.ID,
+                AccountID = dto.AccountID,
+                Name = dto.Name,
+                Accounts = AccountsSelectListItems()
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Update(UpdateCategoryViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Accounts = AccountsSelectListItems();
+                return View(model);
+            }
+
+            var dto = _db.Get<CategoryDTO>(model.ID);
+
+            dto.AccountID = model.AccountID;
+            dto.Name = model.Name;
+
+            _db.InsertOrUpdate(dto);
+
+            _unitOfWork.CommitChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var dto = _db.Get<CategoryDTO>(id);
+
+            _db.Delete(dto);
+
+            _unitOfWork.CommitChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private IEnumerable<SelectListItem> AccountsSelectListItems()
+        {
+            return _db.Query(conn => conn.Query<AccountDTO>("SELECT * FROM Accounts"))
+                .Select(a => new SelectListItem { Value = a.ID.ToString(), Text = a.Name });
+        }
+    }
+}
