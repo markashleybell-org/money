3ba8429cbd7c249aeb6b567039b872e973f57e4c
Mark Bell <me@markb.co.uk>
2017-11-13 06:02:53 +0000

    Implement Party CRUD

---
+++ money.web/Models/Parties/CreatePartyViewModel.cs
@@ -0,0 +1,17 @@
+﻿using money.web.Concrete;
+using money.web.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace money.web.Models
+{
+    public class CreatePartyViewModel
+    {
+        public int AccountID { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<SelectListItem> Accounts { get; set; }
+    }
+}
---
+++ money.web/Models/Parties/UpdatePartyViewModel.cs
@@ -0,0 +1,19 @@
+﻿using money.web.Concrete;
+using money.web.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace money.web.Models
+{
+    public class UpdatePartyViewModel
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int ID { get; set; }
+        public int AccountID { get; set; }
+        public string Name { get; set; }
+
+        public IEnumerable<SelectListItem> Accounts { get; set; }
+    }
+}
