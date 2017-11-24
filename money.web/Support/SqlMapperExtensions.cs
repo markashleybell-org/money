46e387d77ad2de8dc4575cbbb9f79938c5cb244d
Mark Bell <me@markb.co.uk>
2017-11-24 17:47:31 +0000

    Add SqlMapperExtensions (utility extensions for Dapper)

---
+++ money.web/Support/SqlMapperExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using static Dapper.SqlMapper;
+
+namespace money.web.Support
+{
+    public static partial class SqlMapperExtensions
+    {
+        public static IEnumerable<T> QuerySP<T>(this IDbConnection connection, string sql, object param, IDbTransaction transaction = null)
+        {
+            return connection.Query<T>(sql, param, transaction, true, null, CommandType.StoredProcedure);
+        }
+
+        public static GridReader QueryMultipleSP(this IDbConnection connection, string sql, object param)
+        {
+            return connection.QueryMultiple(sql, param, null, null, CommandType.StoredProcedure);
+        }
+
+        public static void ExecuteSP(this IDbConnection connection, string sql, object param, IDbTransaction transaction = null)
+        {
+            connection.Execute(sql, param, transaction, null, CommandType.StoredProcedure);
+        }
+    }
+}
