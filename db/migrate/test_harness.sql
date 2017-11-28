f33856db8ecc294fb95e9bde5803ec141f1fe1bc
Mark Bell <me@markb.co.uk>
2017-11-28 09:03:27 +0000

    Add test harness for migrations

---
+++ db/migrate/test_harness.sql
@@ -0,0 +1,13 @@
+USE [money]
+
+GO
+
+SELECT * FROM Users
+SELECT * FROM Accounts
+SELECT * FROM Categories
+SELECT * FROM Parties
+SELECT * FROM MonthlyBudgets
+SELECT * FROM Categories_MonthlyBudgets
+SELECT * FROM Entries
+
+GO
