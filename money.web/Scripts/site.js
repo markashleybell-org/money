c4c0e596a0311656fd007ea5525d26ce12be1dbb
Mark Bell <me@markb.co.uk>
2017-12-11 07:06:03 +0000

    Begin basic layout of dashboard view

--- money.web/Scripts/site.js
+++ money.web/Scripts/site.js
@@ -1,9 +1,21 @@
 /// <reference types="jquery" />
 var money;
 (function (money) {
-    function init() {
-        console.log('test');
-    }
-    money.init = init;
+    var _accountList;
+    money.init = function () {
+        _accountList = $('.panel-group');
+        _accountList.on('show.bs.collapse hide.bs.collapse', function (e) {
+            var icon = $('#' + e.target.id.replace('categories', 'heading')).find('.account-heading-view-toggle > .fa');
+            if (e.type == 'show') {
+                icon.removeClass('fa-chevron-down').addClass('fa-chevron-up');
+            }
+            else {
+                icon.removeClass('fa-chevron-up').addClass('fa-chevron-down');
+            }
+        });
+    };
 })(money || (money = {}));
+$(function () {
+    money.init();
+});
 //# sourceMappingURL=site.js.map
--- money.web/Scripts/site.ts
+++ money.web/Scripts/site.ts
@@ -1,7 +1,22 @@
 ﻿/// <reference types="jquery" />
 
 namespace money {
-    export function init(): void {
-        console.log('test');
+    let _accountList: JQuery;
+
+    export const init = (): void => {
+        _accountList = $('.panel-group');
+
+        _accountList.on('show.bs.collapse hide.bs.collapse', e => {
+            var icon = $('#' + e.target.id.replace('categories', 'heading')).find('.account-heading-view-toggle > .fa');
+            if (e.type == 'show') {
+                icon.removeClass('fa-chevron-down').addClass('fa-chevron-up');
+            } else {
+                icon.removeClass('fa-chevron-up').addClass('fa-chevron-down');
+            }
+        });
     }
 }
+
+$(() => {
+    money.init();
+});
