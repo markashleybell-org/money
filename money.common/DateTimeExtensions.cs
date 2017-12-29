8d5cdbce3f47da4dbaa7149104c91f6fb61155ca
Mark Bell <me@markb.co.uk>
2017-12-29 08:39:24 +0000

    Use expression-bodied methods

--- money.common/DateTimeExtensions.cs
+++ money.common/DateTimeExtensions.cs
@@ -23,49 +23,22 @@ namespace money.common
             return dt.AddDays(-diff).Date;
         }
 
-        public static DateTime LastDayOfWeek(this DateTime dt)
-        {
-            return dt.FirstDayOfWeek().AddDays(6);
-        }
+        public static DateTime LastDayOfWeek(this DateTime dt) => dt.FirstDayOfWeek().AddDays(6);
 
-        public static DateTime FirstDayOfMonth(this DateTime dt)
-        {
-            return new DateTime(dt.Year, dt.Month, 1);
-        }
+        public static DateTime FirstDayOfMonth(this DateTime dt) => new DateTime(dt.Year, dt.Month, 1);
 
-        public static DateTime LastDayOfMonth(this DateTime dt)
-        {
-            return dt.FirstDayOfMonth().AddMonths(1).AddDays(-1);
-        }
+        public static DateTime LastDayOfMonth(this DateTime dt) => dt.FirstDayOfMonth().AddMonths(1).AddDays(-1);
 
-        public static DateTime FirstDayOfNextMonth(this DateTime dt)
-        {
-            return dt.FirstDayOfMonth().AddMonths(1);
-        }
+        public static DateTime FirstDayOfNextMonth(this DateTime dt) => dt.FirstDayOfMonth().AddMonths(1);
 
-        public static DateTime LastDayOfNextMonth(this DateTime dt)
-        {
-            return dt.FirstDayOfNextMonth().LastDayOfMonth();
-        }
+        public static DateTime LastDayOfNextMonth(this DateTime dt) => dt.FirstDayOfNextMonth().LastDayOfMonth();
 
-        public static DateTime SetTime(this DateTime dt, int hour, int minute, int second)
-        {
-            return new DateTime(dt.Year, dt.Month, dt.Day, hour, minute, second, 0);
-        }
+        public static DateTime SetTime(this DateTime dt, int hour, int minute, int second) => new DateTime(dt.Year, dt.Month, dt.Day, hour, minute, second, 0);
 
-        public static DateTime SetTime(this DateTime dt, int hour, int minute, int second, int millisecond)
-        {
-            return new DateTime(dt.Year, dt.Month, dt.Day, hour, minute, second, millisecond);
-        }
+        public static DateTime SetTime(this DateTime dt, int hour, int minute, int second, int millisecond) => new DateTime(dt.Year, dt.Month, dt.Day, hour, minute, second, millisecond);
 
-        public static DateTime ZeroTime(this DateTime dt)
-        {
-            return dt.SetTime(0, 0, 0, 0);
-        }
+        public static DateTime ZeroTime(this DateTime dt) => dt.SetTime(0, 0, 0, 0);
 
-        public static bool IsLastDayOfMonth(this DateTime dt)
-        {
-            return dt.Date == dt.FirstDayOfMonth().AddMonths(1).AddDays(-1).Date;
-        }
+        public static bool IsLastDayOfMonth(this DateTime dt) => dt.Date == dt.FirstDayOfMonth().AddMonths(1).AddDays(-1).Date;
     }
 }
