1f89412b7a34427f9bc47a8fb3e4fbcdfb19620f
Mark Bell <me@markb.co.uk>
2018-08-08 07:48:35 +0100

    DB creation script is now in /tools

--- new-db.cmd
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-SET SQLCMDPATH=C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE
-
-lprun tools/generate-data-layer.linq
-"%SQLCMDPATH%" -b -i db\schema\kill.sql -v DatabaseName="money"
-"%SQLCMDPATH%" -b -i db\schema\schema.sql -v DatabaseName="money"
-"%SQLCMDPATH%" -b -i db\migrate\migrate.sql -v DatabaseName="money"
---
+++ tools/new-db.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+SET SQLCMDPATH=C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\130\Tools\Binn\SQLCMD.EXE
+
+lprun generate-data-layer.linq
+"%SQLCMDPATH%" -b -i ..\db\schema\kill.sql -v DatabaseName="money"
+"%SQLCMDPATH%" -b -i ..\db\schema\schema.sql -v DatabaseName="money"
