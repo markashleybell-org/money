b3e26cd06a0e40ae1b056bedb48c80d7a938a449
Mark Bell <me@markb.co.uk>
2019-06-28 06:44:43 +0100

    Removed all code relating to PersistentSession

--- db/Tables/PersistentSessions.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[PersistentSessions] (
-    [UserID]           INT            NOT NULL,
-    [SeriesIdentifier] NVARCHAR (256) NOT NULL,
-    [Token]            NVARCHAR (256) NOT NULL,
-    [Created]          DATETIME       NOT NULL,
-    [Expires]          DATETIME       NOT NULL,
-    CONSTRAINT [FK_PersistentSessions_UserID_Users_ID] FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([ID])
-);
-
--- money/Support/Globals.cs
+++ money/Support/Globals.cs
@@ -3,15 +3,7 @@ namespace Money.Support
     public static class Globals
     {
 #pragma warning disable SA1310 // Field names must not contain underscore
-        public const string USER_SESSION_VARIABLE_NAME = "USER_ID";
-
-        public const string PERSISTENT_LOGIN_COOKIE_NAME = "PL";
-
-        public const string PERSISTENT_COOKIE_VALUE_PARSE_ERROR_MESSAGE = "Could not parse persistent session cookie value";
-
         public const string CONTROLLER_SUFFIX = "Controller";
-
-        public const string COOKIE_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 #pragma warning restore SA1310 // Field names must not contain underscore
     }
 }
