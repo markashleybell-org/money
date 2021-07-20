c2dcd99eba9058ad9c233585b13c237e2c679bed
Mark Bell <me@markb.co.uk>
2021-07-20 09:52:06 +0100

    We now depend on Microsoft.Data.SqlClient instead of System.Data.SqlClient

--- money/Support/QueryHelper.cs
+++ money/Support/QueryHelper.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
 using Dapper;
 using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
 using Money.Entities;
 
 namespace Money.Support
